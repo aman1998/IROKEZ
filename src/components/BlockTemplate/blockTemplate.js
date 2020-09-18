bd61e132e7edd6270cbf72803a1ec2bafde2039e
amangeldi <1998-amangeldi@mail.ru>
2020-09-18 09:01:48 +0600

    upgrade block

--- src/components/BlockTemplate/blockTemplate.js
+++ src/components/BlockTemplate/blockTemplate.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './styles.module.scss'
 import Chevron from '../../assets/icons/chevron'
+import ChevronIcon from '../../assets/icons/chevron2.svg'
 
 const BlockTemplate = ({children, title, count}) => {
 // Этот компонент будет служить оберткой для блоков в гланом меню
@@ -10,7 +11,9 @@ const BlockTemplate = ({children, title, count}) => {
         <h2>{title}</h2>
         <span>{count}</span>
       </div>
+      <img src={ChevronIcon} alt='#' className={styles.chevronLeft}/>
       {children}
+      <img src={ChevronIcon} alt='#' className={styles.chevronRight}/>
       {title != 'Водопад событии' ? 
         <div className={styles.blockLink}>
           <div>{title}</div>
@@ -18,6 +21,12 @@ const BlockTemplate = ({children, title, count}) => {
         </div> : 
         <div className={styles.blockLink}></div>
       }
+
+      {/* Точки перехода для слайдера */}
+      <div className={styles.points}>
+        <div className={styles.pointsLeft}></div> 
+        <div className={styles.pointsRight}></div> 
+      </div>
     </div>
   )
 }
--- src/components/RequestBlock/request.js
+++ src/components/RequestBlock/request.js
@@ -3,16 +3,6 @@ import BlockTemplate from '../BlockTemplate/blockTemplate'
 import styles from './styles.module.scss'
 
 const CalendarBlock = () => {
-    const settings = {
-      dots: true,
-      fade: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      arrows: true,
-      className:'sliders'
-    };
   return (
     <BlockTemplate title={'Запросы'} count='1'>
     {/* Блок с запросами */}

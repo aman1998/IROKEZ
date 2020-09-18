35350469d80ce87adaf939f9a26ec84dd8fd642e
amangeldi <1998-amangeldi@mail.ru>
2020-09-18 08:06:42 +0600

    Updates

---
+++ precache-manifest.fca47ffd5aa00e7f5acce4383e7a74b6.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01c70da07f7818679f1c621fa9b1a816",
+    "url": "/aman1998/IROKEZ/index.html"
+  },
+  {
+    "revision": "975d5bc6792067f9e8b0",
+    "url": "/aman1998/IROKEZ/static/css/2.0a1e2970.chunk.css"
+  },
+  {
+    "revision": "0d9c788eaef26e8b361c",
+    "url": "/aman1998/IROKEZ/static/css/main.04382ab7.chunk.css"
+  },
+  {
+    "revision": "975d5bc6792067f9e8b0",
+    "url": "/aman1998/IROKEZ/static/js/2.87f54b0d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/aman1998/IROKEZ/static/js/2.87f54b0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d9c788eaef26e8b361c",
+    "url": "/aman1998/IROKEZ/static/js/main.a4b49dc6.chunk.js"
+  },
+  {
+    "revision": "584d5f90d8d0e480ebf9",
+    "url": "/aman1998/IROKEZ/static/js/runtime-main.c464082b.js"
+  },
+  {
+    "revision": "e63f9d1f209634fb40785e6068329487",
+    "url": "/aman1998/IROKEZ/static/media/1.e63f9d1f.svg"
+  },
+  {
+    "revision": "3c9f3dedc944f22b5bab13d1382f062d",
+    "url": "/aman1998/IROKEZ/static/media/14.3c9f3ded.svg"
+  },
+  {
+    "revision": "df46a7198acb7cef9a66b6f94a3181d6",
+    "url": "/aman1998/IROKEZ/static/media/26.df46a719.svg"
+  },
+  {
+    "revision": "bcb5fa13b77ceebbcfd116fad7e0ffb8",
+    "url": "/aman1998/IROKEZ/static/media/Emote.bcb5fa13.svg"
+  },
+  {
+    "revision": "2f1c4f3c28fa357fa474477cbfcac1e5",
+    "url": "/aman1998/IROKEZ/static/media/Logo.2f1c4f3c.svg"
+  },
+  {
+    "revision": "f72d77eb0767280e1690c425df588986",
+    "url": "/aman1998/IROKEZ/static/media/Updates.f72d77eb.png"
+  },
+  {
+    "revision": "d52203234227eb3c1b16211c326bb634",
+    "url": "/aman1998/IROKEZ/static/media/arrow.d5220323.svg"
+  },
+  {
+    "revision": "37671506c1756e22ef146102b54e393d",
+    "url": "/aman1998/IROKEZ/static/media/avatar.37671506.svg"
+  },
+  {
+    "revision": "53a43efc7b981255ac4721b14986d232",
+    "url": "/aman1998/IROKEZ/static/media/bell.53a43efc.svg"
+  },
+  {
+    "revision": "2645854690ef21563ab1930e32ff2ec0",
+    "url": "/aman1998/IROKEZ/static/media/book.26458546.svg"
+  },
+  {
+    "revision": "b6d83a5c4c9969ba6be8893173cc7fea",
+    "url": "/aman1998/IROKEZ/static/media/chevron.b6d83a5c.svg"
+  },
+  {
+    "revision": "dde9d102cffa174c3dff7cd2a12a05e0",
+    "url": "/aman1998/IROKEZ/static/media/chevron2.dde9d102.svg"
+  },
+  {
+    "revision": "717dde4936d349edd198a92400c46a4b",
+    "url": "/aman1998/IROKEZ/static/media/message.717dde49.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/aman1998/IROKEZ/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/aman1998/IROKEZ/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/aman1998/IROKEZ/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/aman1998/IROKEZ/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "0c1c5a9218b0b96071f7f1b62fbf6e8e",
+    "url": "/aman1998/IROKEZ/static/media/smile.0c1c5a92.svg"
+  }
+]);
